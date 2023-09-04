3d6aac7db71b1215e8e7f2f919db06a86a2f2eb0
jinkwonpark <jk3473@naver.com>
2023-09-04 20:08:00 +0900

    ...

---
+++ src/com/shop/test/DaoTest1.java
@@ -0,0 +1,49 @@
+package com.shop.test;
+
+import com.shop.model.MariaDBCon;
+import com.shop.dto.*;
+import com.shop.model.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaoTest1 {
+    static Connection conn = null;
+    static PreparedStatement pstmt = null;
+    static Statement stmt = null;
+    static ResultSet rs = null;
+
+    public static void main(String[] args) {
+        List<Notice> notiList = new ArrayList<>();
+        DBConnect con = new MariaDBCon();
+        conn = con.connect();
+        if(conn!=null){
+            System.out.println("MySQL 연결 성공");
+        }
+
+        String sql = "select * from notice order by no desc";
+        try {
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while(rs.next()){
+                Notice noti = new Notice();
+                noti.setNo(rs.getInt("no"));
+                noti.setTitle(rs.getString("title"));
+                noti.setContent(rs.getString("content"));
+                noti.setResdate(rs.getString("resdate"));
+                noti.setVisit(rs.getInt("visit"));
+                notiList.add(noti);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            con.close(rs, pstmt, conn);
+        }
+
+        for(Notice n:notiList){
+            System.out.println(n.toString());
+        }
+    }
+}
